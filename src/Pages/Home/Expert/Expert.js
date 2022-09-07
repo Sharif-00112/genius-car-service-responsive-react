ee9da2b5e97aedc63914716a53781a307f90b5a1
Sharif-00112 <sharif.hossain02@northsouth.edu>
2022-09-07 21:15:46 +0600

    firebase auth

--- src/Pages/Home/Expert/Expert.js
+++ src/Pages/Home/Expert/Expert.js
@@ -5,11 +5,11 @@ const Expert = ({ expert }) => {
     return (
         <div className='g-5 col-sm-12 col-md-6 col-lg-4'>
             <div className="card" style={{ width: "18rem" }}>
-                <img src={img} className="card-img-top" alt="..." />
+                <img src={img} className="card-img-top p-2" alt="..." />
                 <div className="card-body">
                     <h5 className="card-title">{name}</h5>
                     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" className="btn btn-primary">Go somewhere</a>
+                    <a href="#" className="btn btn-primary">Details</a>
                 </div>
             </div>
         </div>
